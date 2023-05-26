e9a1984fd6db999ca505c9c157278f83e978e81c
ncolosso <colossonico@gmail.com>
2023-05-26 12:04:52 -0400

    First backend commit

---
+++ backend/splitter-project-backend/src/main/java/com/splitter/SplitterProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.splitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitterProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitterProjectBackendApplication.class, args);
+	}
+
+}
---
+++ backend/splitter-project-backend/src/test/java/com/splitter/SplitterProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.splitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitterProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
