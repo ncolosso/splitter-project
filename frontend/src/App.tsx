5ef196e9b91d8c1580e36755ccf9d4b438c28961
emoltz <es5541@nyu.edu>
2023-05-30 19:42:35 -0400

    custom theme setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,33 @@
 import HomePage from "./components/HomePage.tsx";
 import NavBar from "./components/NavBar";
+import {ThemeProvider, createTheme} from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 
 function App() {
 
-  return (
-    <>
-      <NavBar />
-      <div className="d-flex flex-column p-4 gap-4 py-md-5">
-        <HomePage/>
-      </div>
-    </>
-  );
+    const theme = createTheme({
+        palette: {
+            mode: 'light',
+            primary: {
+                main: "#3767af",
+            },
+            secondary: {
+                main: '#fffbad'
+            }
+        }
+    });
+    return (
+        <>
+            <ThemeProvider theme={theme}>
+                <CssBaseline>
+                    <NavBar/>
+                    <div className="d-flex flex-column p-4 gap-4 py-md-5">
+                        <HomePage/>
+                    </div>
+                </CssBaseline>
+            </ThemeProvider>
+        </>
+    );
 }
 
 export default App;
