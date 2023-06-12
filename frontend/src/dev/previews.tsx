356b3825f8d66ad8de1d7f6cd8217b6f6a97ef6e
emoltz <es5541@nyu.edu>
2023-06-12 14:42:36 -0400

    potential CSS animations

---
+++ frontend/src/dev/previews.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {ComponentPreview, Previews} from "@react-buddy/ide-toolbox";
+import {PaletteTree} from "./palette";
+import BillsList from "../components/BillsList.tsx";
+
+const ComponentPreviews = () => {
+    return (
+        <Previews palette={<PaletteTree/>}>
+            <ComponentPreview path="/BillsList">
+                <BillsList/>
+            </ComponentPreview>
+        </Previews>
+    );
+};
+
+export default ComponentPreviews;
